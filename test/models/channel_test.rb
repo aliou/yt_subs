da711e538e32934989211c13f0188a4816e1fd5c
Aliou Diallo <code@aliou.me>
2014-08-12 15:50:56 +0200

    Test channel model.

--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ChannelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without url" do
+    channel = Channel.create
+    assert_not channel.save
+  end
+
+  test "should not save with invalid url" do
+    channel = Channel.create url: 'www.google.fr'
+    assert_not channel.save
+  end
+
+  test "should fetch the channel title" do
+    channel = Channel.create url: 'youtube.com/fullscreen'
+    assert channel.title
+  end
 end
