7efa7ee409663fb2b8a08920be258f98bc3ec753
Aliou Diallo <code@aliou.me>
2014-08-12 15:42:56 +0200

    Fetch channel title.

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,4 +3,19 @@ require 'yt'
 class Channel < ActiveRecord::Base
   validates :url, presence: true, uniqueness: true
   validates :title, presence: true, uniqueness: true
+
+  before_validation :fetch_title, on: :create
+
+  protected
+  def fetch_title
+    unless self.url.nil?
+      begin
+        channel = Yt::Channel.new url: self.url
+        self.title = channel.title
+      rescue Yt::Error
+        self.title = nil
+        errors.add(:url, "must be a valid youtube channel url")
+      end
+    end
+  end
 end
