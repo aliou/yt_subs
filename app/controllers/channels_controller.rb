aec502a45822c1608207105eeda8fc10afea20b0
Aliou Diallo <code@aliou.me>
2014-09-07 15:09:28 +0200

    Implement Channel controller.

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,15 +1,58 @@
 class ChannelsController < ApplicationController
-  before_action :set_channel, only: [:show]
+  before_action :set_channel, only: [:show, :edit, :update, :destroy]
+
   def index
     @channels = Channel.all
   end
 
   def show
+    @videos = @channel.videos
+  end
+
+  def new
+    @channel = Channel.new
+  end
+
+  def edit
+  end
+
+  def create
+    @channel = Channel.new(channel_params)
+    @channel.last_fetched = 1.day.ago
+
+    if @channel.save
+      redirect_to @channel, 'Channel successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @channel.update(channel_params)
+      redirect_to @channel, notice: 'Channel was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @channel.destroy
+    redirect_to channels_url, notice: 'Channel was successfully destroyed.'
   end
 
   private
 
+  # Private: Set current channel
+  #
+  # Returns Current Channel.
   def set_channel
-    @channel = Channel.where(id: params[:id]).first
+    @channel = Channel.find(params[:id])
+  end
+
+  # Private: Only allow the white listed params through.
+  #
+  # Returns Hash with the white listed params.
+  def channel_params
+    params[:channel]
   end
 end
--- test/controllers/channels_controller_test.rb
+++ test/controllers/channels_controller_test.rb
@@ -5,18 +5,18 @@ class ChannelsControllerTest < ActionController::TestCase
     @channel = channels(:alli)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:channels)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create channel" do
+  test 'should create channel' do
     assert_difference('Channel.count') do
       post :create, channel: {  }
     end
@@ -24,22 +24,22 @@ class ChannelsControllerTest < ActionController::TestCase
     assert_redirected_to channel_path(assigns(:channel))
   end
 
-  test "should show channel" do
+  test 'should show channel' do
     get :show, id: @channel
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @channel
     assert_response :success
   end
 
-  test "should update channel" do
+  test 'should update channel' do
     patch :update, id: @channel, channel: {  }
     assert_redirected_to channel_path(assigns(:channel))
   end
 
-  test "should destroy channel" do
+  test 'should destroy channel' do
     assert_difference('Channel.count', -1) do
       delete :destroy, id: @channel
     end
