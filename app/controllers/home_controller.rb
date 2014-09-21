38d2774fb0f7a01f2cd0c1c8f3aa2cf370679e2d
Aliou Diallo <code@aliou.me>
2014-09-21 20:17:37 +0200

    Document controller method.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  # Show the last five videos on the homepage.
   def index
     @videos = Video.where(date: 1.day.ago..Time.now)
       .order(date: :desc).take(5)
