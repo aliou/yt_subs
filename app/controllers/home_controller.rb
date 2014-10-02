87e68c521df9fc68efa2b437ed065bc42b92f4c6
Aliou Diallo <code@aliou.me>
2014-10-02 09:43:43 +0200

    Fix typo and make HoundCI happy.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
-
-  # Show the last nine videos on the homepage by group of 3.
+  # Show the last nine videos on the homepage by groups of 3.
   def index
     @videos = Video.order(date: :desc).take(9).each_slice(3).to_a
   end
