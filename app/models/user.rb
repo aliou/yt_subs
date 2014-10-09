b4a74d5256d27c9d98569f5a897516c2c59500f0
Aliou Diallo <code@aliou.me>
2014-10-09 09:30:04 +0200

    Create a user from OmniAuth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ActiveRecord::Base
   attr_accessor :name, :email
 
   validates :name, presence: true
+
+  # Create a user from its omniauth authentification details.
+  #
+  # auth - The omniauth authentification details.
+  #
+  # Returns self.
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+        user.name = auth['info']['name'] || ""
+      end
+    end
+  end
+
 end
