b9c4cb6e56af251e3038c59153f3d9f8e619a48e
Aliou Diallo <code@aliou.me>
2014-10-09 15:40:01 +0200

    Save Oauth token and secret with a new user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,15 @@ class User < ActiveRecord::Base
   # Returns self.
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.provider = auth['provider']
-      user.uid = auth['uid']
+      user.provider     = auth['provider']
+      user.uid          = auth['uid']
+      user.oauth_token  = auth['credentials']['token']
+      user.oauth_secret = auth['credentials']['secret']
+
       if auth['info']
-        user.name = auth['info']['name'] || ""
+        user.name       = auth['info']['name'] || ""
       end
+
     end
   end
-
 end
