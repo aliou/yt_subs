38380982b50b7fba4e0673753b33eff08e8520ba
Aliou Diallo <code@aliou.me>
2014-10-09 15:48:12 +0200

    Make sure the user is connected.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Make sure the user is connected.
+  before_filter :authenticate!
+
+  private
+
+  def authenticate!
+    redirect_to signin_path unless session[:user_id].present?
+  end
 end
