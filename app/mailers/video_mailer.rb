92420ce03e893dd755b23c11959040518c3d5496
Aliou Diallo <code@aliou.me>
2014-10-10 20:27:01 +0200

    Move the email recipient inside method.

--- app/mailers/video_mailer.rb
+++ app/mailers/video_mailer.rb
@@ -2,11 +2,10 @@ class VideoMailer < ActionMailer::Base
   include Roadie::Rails::Automatic
 
   default from: "YT <#{ENV['BOT_EMAIL']}>"
-  default to:   User.first.email
 
   # Send the videos
   def send_videos(videos)
     @videos = videos
-    mail subject: "YT new videos for #{ Date.today.to_s }"
+    mail subject: "YT new videos for #{ Date.today.to_s }", to: User.first.email
   end
 end
